a30701149fea861944b155cbb695149b346ae3fe
woldier <1098582358@qq.com>
2023-02-15 12:59:12 +0800

    添加了content-service 的mapper类中的mapper注解

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMapper.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMapper.java
@@ -2,6 +2,7 @@ package com.xuecheng.content.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xuecheng.content.model.po.Teachplan;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -10,6 +11,7 @@ import com.xuecheng.content.model.po.Teachplan;
  *
  * @author itcast
  */
+@Mapper
 public interface TeachplanMapper extends BaseMapper<Teachplan> {
 
 }
