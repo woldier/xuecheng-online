dd51bb037edbb76ac3a1d59116b95e9a3f16b6d6
woldier <1098582358@qq.com>
2023-02-15 12:51:24 +0800

    修改了content-service 的mapper类的包名

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMapper.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMapper.java
@@ -1,4 +1,4 @@
-package com.xuecheng.content.service.mapper;
+package com.xuecheng.content.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xuecheng.content.model.po.Teachplan;
