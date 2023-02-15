91cefd98d12b9d898278fdedb464cac47d88e436
woldier <1098582358@qq.com>
2023-02-15 12:12:44 +0800

    添加了content-service 的spring boot 启动类

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/App.java
@@ -0,0 +1,17 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author woldier
+ * @version 1.0
+ * @description content 服务 启动类
+ * @date 2023/2/15 12:03
+ **/
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
