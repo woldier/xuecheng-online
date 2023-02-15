779c06bcdfa518d9db8f0b2757d1bdec9f3563b8
woldier <1098582358@qq.com>
2023-02-15 10:13:01 +0800

    content-model模块QueryCourseParamsDto添加swagger-model注解

--- xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/QueryCourseParamsDto.java
+++ xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/QueryCourseParamsDto.java
@@ -1,5 +1,6 @@
 package com.xuecheng.content.model.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.ToString;
 
@@ -11,9 +12,12 @@ import lombok.ToString;
  */
 @Data
 @ToString
-public class QueryCourseParamsDto { //审核状态
+public class QueryCourseParamsDto {
+    //审核状态
+    @ApiModelProperty("审核状态")
     private String auditStatus;
     //课程名称
+    @ApiModelProperty("课程名称")
     private String courseName;
     //发布状态
     private String publishStatus;
