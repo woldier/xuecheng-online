f6c2b4f6f5011c584a7109ee9c0239f5bea537ef
woldier <1098582358@qq.com>
2023-02-15 22:41:25 +0800

    添加了course service的course service 接口的测试

--- xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/CourseBaseMapperTest.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/CourseBaseMapperTest.java
@@ -1,7 +1,12 @@
 package com.xuecheng.content;
 
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.mapper.CourseBaseMapper;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.service.CourseBaseInfoService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +19,22 @@ import org.springframework.boot.test.context.SpringBootTest;
  * @date 2023/2/15 12:53
  **/
 @SpringBootTest
+@Slf4j
 public class CourseBaseMapperTest {
     @Autowired
     CourseBaseMapper courseBaseMapper;
+
+    @Autowired
+    CourseBaseInfoService courseBaseInfoService;
     @Test
     void testCourseBaseMapper() {
         CourseBase courseBase = courseBaseMapper.selectById(74L);
         Assertions.assertNotNull(courseBase);
+    }
+
+    @Test
+    void testCourseBaseInfoService() {
+        PageResult<CourseBase> courseBasePageResult = courseBaseInfoService.queryCourseBaseList(new PageParams(1, 5), new QueryCourseParamsDto());
+        log.info(courseBasePageResult.toString());
     }
 }
