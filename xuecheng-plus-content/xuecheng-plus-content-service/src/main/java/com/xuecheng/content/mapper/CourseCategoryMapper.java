d7498d9fb229485213c00c198c1e775123561eca
woldier <1098582358@qq.com>
2023-02-16 20:53:53 +0800

    添加了content-service模块课程分类的dao层代码

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/CourseCategoryMapper.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/CourseCategoryMapper.java
@@ -1,7 +1,11 @@
 package com.xuecheng.content.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuecheng.content.model.dto.CourseCategoryTreeDto;
 import com.xuecheng.content.model.po.CourseCategory;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 /**
  * <p>
@@ -10,6 +14,14 @@ import com.xuecheng.content.model.po.CourseCategory;
  *
  * @author itcast
  */
+@Mapper
 public interface CourseCategoryMapper extends BaseMapper<CourseCategory> {
-
+    /**
+    * @description 根据id 查询所有子节点
+    * @param id
+    * @return java.util.List<com.xuecheng.content.model.dto.CourseCategoryTreeDto>
+    * @author: woldier
+    * @date: 2023/2/16 20:38
+    */
+    List<CourseCategoryTreeDto> selectTreeNodes(String id);
 }
