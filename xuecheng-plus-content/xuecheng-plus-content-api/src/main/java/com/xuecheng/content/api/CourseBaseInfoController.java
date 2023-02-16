2bdf2c106d4748b139221c31f567dba4b042d5bb
woldier <1098582358@qq.com>
2023-02-16 08:56:53 +0800

    完善了content api controller中，查询课程信息list的接口

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -4,8 +4,10 @@ import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.po.CourseBase;
 //import com.xuecheng.content.model.vo.QueryCourseParamsDto;
+import com.xuecheng.content.service.CourseBaseInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,17 +25,16 @@ import java.util.List;
  **/
 @Api(value = "课程信息编辑接口", tags = "课程信息编辑接口")
 @RestController
+@RequiredArgsConstructor // lombok bean注入
 public class CourseBaseInfoController {
+    /*
+    通过Lombok生成构造方法进行注入
+     */
+    private final CourseBaseInfoService courseBaseInfoService;
     @ApiOperation("课程查询接口")
     @PostMapping("/course/list")
     public PageResult<CourseBase> list(PageParams pageParams, @RequestBody
     QueryCourseParamsDto queryCourseParams) {
-        CourseBase courseBase = new CourseBase();
-        courseBase.setName("测试名称");
-        courseBase.setCreateDate(LocalDateTime.now());
-        List<CourseBase> courseBases = new ArrayList();
-        courseBases.add(courseBase);
-        PageResult pageResult = new PageResult<CourseBase>(courseBases,10,1,10);
-        return pageResult;
+        return courseBaseInfoService.queryCourseBaseList(pageParams,queryCourseParams);
     }
 }
