d7845daae4e8ee0f0ba70cde00f8755c22c4eaf7
woldier <1098582358@qq.com>
2023-03-07 19:08:49 +0800

    content-api 完善了课程计划查询代码

---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/TeachplanController.java
@@ -0,0 +1,33 @@
+package com.xuecheng.content.api;
+
+import com.xuecheng.content.model.dto.TeachplanDto;
+import com.xuecheng.content.service.TeachplanService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author woldier
+ * @version 1.0
+ * @description 课程计划相关接口
+ * @date 2023/3/7 17:28
+ **/
+@Api(value = "课程计划编辑接口",tags = "课程计划编辑接口")
+@RestController
+@RequiredArgsConstructor
+public class TeachplanController {
+
+    private final TeachplanService teachplanService;
+    @ApiOperation("查询课程计划树形结构")
+    @ApiImplicitParam(value = "courseId",name = "课程Id",required = true,dataType = "Long",paramType = "path")
+    @GetMapping("/teachplan/{courseId}/tree-nodes")
+    public List<TeachplanDto> getTreeNodes(@PathVariable Long courseId){
+        return teachplanService.selectTreeNodes(courseId);
+    }
+}
