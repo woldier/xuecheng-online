d47fea1ad5f75a9893743f0185dbfeebf65becb4
woldier <1098582358@qq.com>
2023-03-07 18:38:46 +0800

    content-service 课程计划mapper接口测试

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/TeachplanMapperTest.java
@@ -0,0 +1,30 @@
+package com.xuecheng.content;
+
+import com.xuecheng.content.mapper.TeachplanMapper;
+import com.xuecheng.content.model.dto.TeachplanDto;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+/**
+ * @author woldier
+ * @version 1.0
+ * @description TODO
+ * @date 2023/3/7 17:54
+ **/
+@SpringBootTest
+
+@Slf4j
+public class TeachplanMapperTest {
+    @Autowired
+    private TeachplanMapper teachplanMapper;
+
+    @Test
+    public void test(){
+        List<TeachplanDto> teachplanDtos = teachplanMapper.selectTreeNodes(117L);
+        log.info(teachplanDtos.toString());
+    }
+}
