b07bcf9aade4f1644a49656b45f9ab889bf575f3
woldier <1098582358@qq.com>
2023-03-07 18:38:29 +0800

    content-service 课程计划mapper接口

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMapper.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMapper.java
@@ -1,8 +1,11 @@
 package com.xuecheng.content.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuecheng.content.model.dto.TeachplanDto;
 import com.xuecheng.content.model.po.Teachplan;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,5 +16,12 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface TeachplanMapper extends BaseMapper<Teachplan> {
-
+    /**
+    * @description 根据课程id查询课程计划信息和课程媒体信息
+    * @param courseId
+    * @return java.util.List<com.xuecheng.content.model.dto.TeachplanDto>
+    * @author: woldier
+    * @date: 2023/3/7 17:50
+    */
+    List<TeachplanDto> selectTreeNodes(Long courseId);
 }
