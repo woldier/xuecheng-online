d2daebb96a391da9a1e3bd1f89d13aed994c39d9
woldier <1098582358@qq.com>
2023-03-08 12:02:02 +0800

    content-api CourseTeacherController 课程教师信息删除接口层代码

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseTeacherController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseTeacherController.java
@@ -26,12 +26,12 @@ public class CourseTeacherController {
     private final CourseTeacherService courseTeacherService;
 
     /**
-    * @description 根据课程id查询教师
-    * @param courseId  课程id
-    * @return java.util.List<com.xuecheng.content.model.po.CourseTeacher>
-    * @author: woldier
-    * @date: 2023/3/8 8:55
-    */
+     * @param courseId 课程id
+     * @return java.util.List<com.xuecheng.content.model.po.CourseTeacher>
+     * @description 根据课程id查询教师
+     * @author: woldier
+     * @date: 2023/3/8 8:55
+     */
     @ApiOperation("根据课程id查询课程教师信息")
     @GetMapping("/courseTeacher/list/{courseId}")
     public List<CourseTeacher> list(@PathVariable @NotNull(message = "课程id不能为空") Long courseId) throws XueChengPlusException {
@@ -39,16 +39,30 @@ public class CourseTeacherController {
     }
 
     /**
-    * @description 新增/修改教师信息
-    * @param dto  请求参数
-    * @return void
-    * @author: woldier
-    * @date: 2023/3/8 9:48
-    */
+     * @param dto 请求参数
+     * @return void
+     * @description 新增/修改教师信息
+     * @author: woldier
+     * @date: 2023/3/8 9:48
+     */
     @ApiOperation("新增/修改课程教师信息")
     @PostMapping("/courseTeacher")
     public void saveOrUpdate(@RequestBody @Validated TeacherSaveOrUpdateDto dto) throws XueChengPlusException {
         courseTeacherService.saveOrUpdateTeacher(dto);
+    }
+
+    /**
+     * @param courseId 课程id
+     * @param id       教师id
+     * @return void
+     * @description 删除课程教师
+     * @author: woldier
+     * @date: 2023/3/8 10:35
+     */
+    @ApiOperation("新增/修改课程教师信息")
+    @DeleteMapping("/courseTeacher/course/{courseId}/{id}")
+    public void deleteById(@PathVariable Long courseId, @PathVariable Long id) throws XueChengPlusException {
+        courseTeacherService.deleteTeacher(courseId,id);
     }
 
 
