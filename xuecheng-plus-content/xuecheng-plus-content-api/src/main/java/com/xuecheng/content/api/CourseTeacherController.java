67fb6f3b5c10afd91d456cae328a5fedad4ad90b
woldier <1098582358@qq.com>
2023-03-08 10:24:24 +0800

    content-service CourseTeacherController 课程教师信息新增/修改的接口层代码

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseTeacherController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseTeacherController.java
@@ -1,12 +1,15 @@
 package com.xuecheng.content.api;
 
 import com.xuecheng.base.exception.XueChengPlusException;
+import com.xuecheng.content.model.dto.TeacherSaveOrUpdateDto;
 import com.xuecheng.content.model.po.CourseTeacher;
 import com.xuecheng.content.service.CourseTeacherService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
@@ -17,7 +20,6 @@ import java.util.List;
  * @date 2023/3/8 8:52
  **/
 @RestController
-@RequestMapping("/courseTeacher")
 @RequiredArgsConstructor
 public class CourseTeacherController {
 
@@ -31,8 +33,23 @@ public class CourseTeacherController {
     * @date: 2023/3/8 8:55
     */
     @ApiOperation("根据课程id查询课程教师信息")
-    @GetMapping("/list/{courseId}")
+    @GetMapping("/courseTeacher/list/{courseId}")
     public List<CourseTeacher> list(@PathVariable @NotNull(message = "课程id不能为空") Long courseId) throws XueChengPlusException {
         return courseTeacherService.listTeacherByCourseId(courseId);
     }
+
+    /**
+    * @description 新增/修改教师信息
+    * @param dto  请求参数
+    * @return void
+    * @author: woldier
+    * @date: 2023/3/8 9:48
+    */
+    @ApiOperation("新增/修改课程教师信息")
+    @PostMapping("/courseTeacher")
+    public void saveOrUpdate(@RequestBody @Validated TeacherSaveOrUpdateDto dto) throws XueChengPlusException {
+        courseTeacherService.saveOrUpdateTeacher(dto);
+    }
+
+
 }
