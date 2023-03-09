3ca6effbfe07f2d7c950c1d163a175164802381f
woldier <1098582358@qq.com>
2023-03-09 20:22:55 +0800

    media-service 中不存在启动类,为了便于单元测试创建了test的启动类

---
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/test/java/com/xuecheng/media/MediaServiceApp.java
@@ -0,0 +1,17 @@
+package com.xuecheng.media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author woldier
+ * @version 1.0
+ * @description media -service Test启动类
+ * @date 2023/3/9 19:05
+ **/
+@SpringBootApplication
+public class MediaServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MediaServiceApp.class,args);
+    }
+}
