1bffbe69f34f73d358e3d82b8568062584c2828a
woldier <1098582358@qq.com>
2023-03-09 22:22:08 +0800

    media-api 文件上传接口返回的Dto

---
+++ xuecheng-plus-media/xuecheng-plus-media-model/src/main/java/com/xuecheng/media/model/dto/UploadFileResultDto.java
@@ -0,0 +1,12 @@
+package com.xuecheng.media.model.dto;
+
+import com.xuecheng.media.model.po.MediaFiles;
+
+/**
+ * @author woldier
+ * @version 1.0
+ * @description 上传文件响应结果类
+ * @date 2023/3/9 21:55
+ **/
+public class UploadFileResultDto extends MediaFiles {
+}
