1a43df5b35c74ae64321b188095ae2af9ac1b11d
woldier <1098582358@qq.com>
2023-03-10 17:34:18 +0800

    media-service MediaApplication.java 开启AspectJ并且暴露出AopContext

--- xuecheng-plus-media/xuecheng-plus-media-api/src/main/java/com/xuecheng/MediaApplication.java
+++ xuecheng-plus-media/xuecheng-plus-media-api/src/main/java/com/xuecheng/MediaApplication.java
@@ -4,9 +4,11 @@ package com.xuecheng;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableSwagger2Doc
 @SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true,proxyTargetClass = true)
 public class MediaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MediaApplication.class, args);
