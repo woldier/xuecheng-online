951704ab2f9e734a2748cd817b530cf2f98c11eb
woldier <1098582358@qq.com>
2023-03-27 15:49:18 +0800

    media-service MediaFileService 新增上传课程静态化页面业务

--- xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/impl/MediaFileServiceImpl.java
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/impl/MediaFileServiceImpl.java
@@ -606,7 +606,7 @@ public class MediaFileServiceImpl extends ServiceImpl<MediaFilesMapper, MediaFil
     @Override
     public void uploadCourseHtml(String localFilePath, String objectName) {
         //所有的静态文件都存在course文件夹下
-        minIOUpload(localFilePath, getMimeType(objectName), fileBucket, "course/"+objectName);
+        minIOUpload(localFilePath, getMimeType(objectName), fileBucket, objectName);
     }
 
     /**
