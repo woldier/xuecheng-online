8e356caa2355b9e41be9e81d16e27d89143305cc
woldier <1098582358@qq.com>
2023-03-27 15:37:14 +0800

    media-api MediaFilesController.java 新增上传课程静态化页面接口

--- xuecheng-plus-media/xuecheng-plus-media-api/src/main/java/com/xuecheng/media/api/MediaFilesController.java
+++ xuecheng-plus-media/xuecheng-plus-media-api/src/main/java/com/xuecheng/media/api/MediaFilesController.java
@@ -1,5 +1,7 @@
 package com.xuecheng.media.api;
 
+import com.j256.simplemagic.ContentInfo;
+import com.j256.simplemagic.ContentInfoUtil;
 import com.xuecheng.base.exception.XueChengPlusException;
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
@@ -11,6 +13,8 @@ import com.xuecheng.media.model.po.MediaFiles;
 import com.xuecheng.media.service.MediaFileService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.MediaType;
@@ -27,6 +31,7 @@ import java.io.IOException;
  */
 @Api(value = "媒资文件管理接口", tags = "媒资文件管理接口")
 @RestController
+@Slf4j
 public class MediaFilesController {
 
 
@@ -43,9 +48,7 @@ public class MediaFilesController {
     }
 
     /**
-     * @param upload     表单数据
-     * @param folder     文件夹名-非必须
-     * @param objectName 文件名-非必须
+     * @param upload 表单数据
      * @return com.xuecheng.media.model.dto.UploadFileResultDto
      * @description 文件上传
      * @author: woldier
@@ -55,21 +58,18 @@ public class MediaFilesController {
     @RequestMapping(value = "/upload/coursefile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     // 定义请求url与可消费的文件操作content-type类型
     public UploadFileResultDto upload(
-            @RequestPart("filedata") MultipartFile upload,
-            @RequestParam(value = "folder", required = false) String folder,
-            @RequestParam(value = "objectName", required = false) String objectName) throws IOException, XueChengPlusException {
+            @RequestPart("filedata") MultipartFile upload
+    ) throws IOException, XueChengPlusException {
         /*
-        *对接受到的文件进行处理
+         *对接受到的文件进行处理
          */
         /*产生一个临时文件*/
-        File tempFile = File.createTempFile("minio", "temp");
-        /*将请求中的表单数据拷贝到临时文件中*/
-        upload.transferTo(tempFile);
+        File tempFile = getTempFile(upload);
         /*获取绝对路径*/
         String absolutePath = tempFile.getAbsolutePath();
 
         /*
-        *对上传参数进行处理
+         *对上传参数进行处理
          */
 
         //上传文件参数类
@@ -85,8 +85,8 @@ public class MediaFilesController {
 
 
         /*
-        * 公司id获取
-        * */
+         * 公司id获取
+         * */
         //TODO 硬编码公司id
         Long companyId = getCompanyId();
 
@@ -97,7 +97,29 @@ public class MediaFilesController {
         return uploadFileResultDto;
     }
 
-    private long getCompanyId(){
+    @NotNull
+    private static File getTempFile(MultipartFile upload) throws IOException {
+        File tempFile = File.createTempFile("minio", "temp");
+        /*将请求中的表单数据拷贝到临时文件中*/
+        upload.transferTo(tempFile);
+        return tempFile;
+    }
+
+    @ApiOperation("静态化网页上传")
+    @RequestMapping(value = "/upload/coursehtml", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void uploadHtml(
+            @RequestPart("filedata") MultipartFile upload,
+            @RequestParam(value = "objectName", required = false) String objectName) throws IOException {
+        File tempFile = getTempFile(upload);
+        mediaFileService.uploadCourseHtml(tempFile.getAbsolutePath(), objectName);
+        boolean delete = tempFile.delete();
+        if (!delete)
+            log.error("删除文件错误{}", tempFile.getAbsolutePath());
+    }
+
+    private long getCompanyId() {
         return 1232141425L;
     }
+
+
 }
