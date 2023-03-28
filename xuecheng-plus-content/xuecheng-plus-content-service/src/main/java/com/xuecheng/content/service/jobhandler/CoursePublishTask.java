ebd37cdd5f0c15bf9e628a5b30b0f706c9190b21
woldier <1098582358@qq.com>
2023-03-28 16:04:35 +0800

    content-service CoursePublishTask 更新插入es索引相关代码逻辑

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/jobhandler/CoursePublishTask.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/jobhandler/CoursePublishTask.java
@@ -1,6 +1,9 @@
 package com.xuecheng.content.service.jobhandler;
 
 import com.xuecheng.base.exception.XueChengPlusException;
+import com.xuecheng.content.feignclient.search.CourseIndex;
+import com.xuecheng.content.feignclient.search.SearchCourseIndexClient;
+import com.xuecheng.content.model.po.CoursePublish;
 import com.xuecheng.content.service.CoursePublishService;
 import com.xuecheng.messagesdk.model.po.MqMessage;
 import com.xuecheng.messagesdk.service.MessageProcessAbstract;
@@ -10,6 +13,7 @@ import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -35,6 +39,8 @@ public class CoursePublishTask extends MessageProcessAbstract {
      */
 
     private final CoursePublishService coursePublishService;
+    private final SearchCourseIndexClient searchCourseIndexClient;
+
     @XxlJob("CoursePublishJobHandler") //任务名
     public void coursePublishJobHandler() throws Exception {
         // 分片参数
@@ -60,18 +66,27 @@ public class CoursePublishTask extends MessageProcessAbstract {
         //取出对应的course 这里我们约定课程id存在于message表的 businessKey1 字段
         Integer courseId = Integer.valueOf(mqMessage.getBusinessKey1());
 
-        //执行阶段1 ,静态化页面
-        generateCourseHtml(mqMessage,courseId);
-        //执行阶段2 , 写入 elasticsearch
-        saveCourseIndex(mqMessage,courseId);
-        //执行阶段3 , 写入redis
-        saveCourseCache(mqMessage,courseId);
 
+        try {
+            if (!Boolean.parseBoolean(mqMessage.getStageState1()))
+                //执行阶段1 ,静态化页面
+                generateCourseHtml(mqMessage, courseId);
+            //执行阶段2 , 写入 elasticsearch
+            if (!Boolean.parseBoolean(mqMessage.getStageState2()))
+                saveCourseIndex(mqMessage, courseId);
+            if (!Boolean.parseBoolean(mqMessage.getStageState3()))
+                //执行阶段3 , 写入redis
+                saveCourseCache(mqMessage, courseId);
+        } catch (XueChengPlusException e) {
+            log.error("执行任务出错{}", e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
         return true;
     }
 
     //生成课程静态化页面并上传至文件系统
-    public void generateCourseHtml(MqMessage mqMessage, long courseId) {
+    public void generateCourseHtml(MqMessage mqMessage, long courseId) throws XueChengPlusException {
 
         log.debug("开始进行课程静态化,课程id:{}", courseId);
         //消息id
@@ -85,14 +100,10 @@ public class CoursePublishTask extends MessageProcessAbstract {
             return;
         }
         //生成静态页面
-        try {
-            File file = coursePublishService.generateCourseHtml(courseId);
-            coursePublishService.uploadCourseHtml(courseId,file);
-            file.delete();
-        } catch (XueChengPlusException e) {
-            log.error("静态化页面出错");
-            throw new RuntimeException(e);
-        }
+
+        File file = coursePublishService.generateCourseHtml(courseId);
+        coursePublishService.uploadCourseHtml(courseId, file);
+        file.delete();
 
 
         //保存第一阶段状态
@@ -114,17 +125,21 @@ public class CoursePublishTask extends MessageProcessAbstract {
     }
 
     //保存课程索引信息
-    public void saveCourseIndex(MqMessage mqMessage, long courseId) {
+    public void saveCourseIndex(MqMessage mqMessage, long courseId) throws XueChengPlusException {
         log.debug("保存课程索引信息,课程id:{}", courseId);
 
-        // TODO 保存课程索引信息
-        try {
-            TimeUnit.SECONDS.sleep(2);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        CoursePublish coursePublish = coursePublishService.getById(courseId);
+        CourseIndex courseIndex = new CourseIndex();
+        BeanUtils.copyProperties(coursePublish, courseIndex);
+
+
+        if (!searchCourseIndexClient.add(courseIndex)) {
+            log.error("添加索引失败");
+            XueChengPlusException.cast("添加索引失败");
         }
-
-
+        //保存第二阶段状态
+        MqMessageService mqMessageService = this.getMqMessageService();
+        mqMessageService.completedStageTwo(courseId);
     }
 
 
