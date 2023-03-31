7a233a4914e9687731a962d2ae665c81692d5605
woldier <1098582358@qq.com>
2023-03-31 19:41:36 +0800

    auth模块 LoginController.java 权限控制注解

--- xuecheng-plus-auth/src/main/java/com/xuecheng/auth/controller/LoginController.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/auth/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.xuecheng.ucenter.mapper.XcUserMapper;
 import com.xuecheng.ucenter.model.po.XcUser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,11 +37,13 @@ public class LoginController {
     }
 
     @RequestMapping("/r/r1")
+    @PreAuthorize(value = "hasAuthority('p1')")
     public String r1() {
         return "访问r1资源";
     }
 
     @RequestMapping("/r/r2")
+    @PreAuthorize(value = "hasAuthority('p2')")
     public String r2() {
         return "访问r2资源";
     }
