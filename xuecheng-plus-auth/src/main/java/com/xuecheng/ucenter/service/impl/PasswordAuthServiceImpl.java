bb825d1bde49e5ec8dabc7dcffffe3f96ed3b1c1
woldier <1098582358@qq.com>
2023-04-05 09:59:47 +0800

    auth 鉴权实现类 PasswordAuthServiceImpl 实现通过密码认证

--- xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/PasswordAuthServiceImpl.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/PasswordAuthServiceImpl.java
@@ -1,6 +1,7 @@
 package com.xuecheng.ucenter.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.xuecheng.ucenter.feignclient.CheckCodeClient;
 import com.xuecheng.ucenter.mapper.XcUserMapper;
 import com.xuecheng.ucenter.model.dto.AuthParamsDto;
 import com.xuecheng.ucenter.model.dto.XcUserExt;
@@ -10,6 +11,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 /**
  * @author woldier
@@ -25,23 +27,34 @@ public class PasswordAuthServiceImpl implements AuthService {
     @Autowired
     PasswordEncoder passwordEncoder;
 
+    @Autowired
+    CheckCodeClient checkCodeClient;
+
     @Override
     public XcUserExt authentic(AuthParamsDto authParamsDto) {
+        /*校验验证码*/
+        if (StringUtils.isEmpty(authParamsDto.getCheckcode()) ||
+                StringUtils.isEmpty(authParamsDto.getCheckcodekey()))
+            throw new RuntimeException("验证码为空");
+
+        if (!checkCodeClient.verify(authParamsDto.getCheckcodekey(), authParamsDto.getCheckcode())) {
+            throw new RuntimeException("验证码校验失败");
+        }
         //账号
         String username = authParamsDto.getUsername();
         XcUser user = xcUserMapper.selectOne(new LambdaQueryWrapper<XcUser>().eq(XcUser::getUsername, username));
-        if(user==null){
+        if (user == null) {
             //返回空表示用户不存在
             throw new RuntimeException("账号不存在");
         }
         XcUserExt xcUserExt = new XcUserExt();
-        BeanUtils.copyProperties(user,xcUserExt);
+        BeanUtils.copyProperties(user, xcUserExt);
         //校验密码
         //取出数据库存储的正确密码
-        String passwordDb  =user.getPassword();
+        String passwordDb = user.getPassword();
         String passwordForm = authParamsDto.getPassword();
         boolean matches = passwordEncoder.matches(passwordForm, passwordDb);
-        if(!matches){
+        if (!matches) {
             throw new RuntimeException("账号或密码错误");
         }
         return xcUserExt;
