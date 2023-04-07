c5d0ef97ece211a11f842f9ba8cf0f3673e475c4
woldier <1098582358@qq.com>
2023-04-07 12:59:14 +0800

    auth LoginController.register 注册接口

--- xuecheng-plus-auth/src/main/java/com/xuecheng/auth/controller/LoginController.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/auth/controller/LoginController.java
@@ -1,13 +1,16 @@
 package com.xuecheng.auth.controller;
 
+import com.xuecheng.auth.exception.XueChengPlusException;
+import com.xuecheng.ucenter.feignclient.CheckCodeClient;
 import com.xuecheng.ucenter.mapper.XcUserMapper;
+import com.xuecheng.ucenter.model.dto.RegisterDto;
 import com.xuecheng.ucenter.model.po.XcUser;
+import com.xuecheng.ucenter.service.RegisterService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Mr.M
@@ -21,14 +24,33 @@ public class LoginController {
 
     @Autowired
     XcUserMapper userMapper;
+    @Autowired
+    CheckCodeClient checkCodeClient;
 
-
+    @Autowired
+    RegisterService registerService;
     @RequestMapping("/login-success")
     public String loginSuccess() {
-
         return "登录成功";
     }
 
+
+    /**
+    *
+    * description 注册
+    *
+    *
+    * @return java.lang.String
+    * @author: woldier
+    * @date: 2023/4/6 19:25
+    */
+    @PostMapping ("/user/doregister")
+    public String register(@RequestBody @Validated RegisterDto registerDto) throws XueChengPlusException {
+        Boolean verify = checkCodeClient.verify(registerDto.getCheckcodekey(), registerDto.getCheckcode());
+        if(!verify) XueChengPlusException.cast("效验错误");
+        boolean b = registerService.registerUser(registerDto);
+        return "登录成功";
+    }
 
     @RequestMapping("/user/{id}")
     public XcUser getuser(@PathVariable("id") String id) {
