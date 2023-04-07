0646de465e9d0bed10e11cdd7644ce8a9860ec66
woldier <1098582358@qq.com>
2023-04-07 23:05:30 +0800

    auth 验证group

---
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/auth/exception/ValidationGroups.java
@@ -0,0 +1,13 @@
+package com.xuecheng.auth.exception;
+
+/**
+ * @author woldier
+ * @version 1.0
+ * @description Validation 校验分组
+ * @date 2023/3/6 17:32
+ **/
+public class ValidationGroups {
+    public interface Inster{};
+    public interface Update{};
+    public interface Delete{};
+}
