b6c9fe2530ca6c0f8f2c4e64084da0c42d0a76f5
woldier <1098582358@qq.com>
2023-04-07 23:06:27 +0800

    auth 用户注册与找回密码dto

--- xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/model/dto/RegisterDto.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/model/dto/RegisterDto.java
@@ -1,5 +1,6 @@
 package com.xuecheng.ucenter.model.dto;
 
+import com.xuecheng.auth.exception.ValidationGroups;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
@@ -15,49 +16,54 @@ public class RegisterDto {
     /**
      * 电话
      */
-    @NotEmpty(message = "电话号码不能为空")
+    @NotEmpty(groups = {ValidationGroups.Inster.class}, message = "电话号码不能为空")
     private String cellphone;
     /**
      * 校验码
      */
-    @NotEmpty(message = "电校验码不能为空")
+    @NotEmpty(
+            groups = {
+                    ValidationGroups.Inster.class,
+                    ValidationGroups.Update.class},
+            message = "电校验码不能为空"
+    )
 
     private String checkcode;
     /**
      * 校验码key
      */
-    @NotEmpty(message = "校验码key不能为空")
+    @NotEmpty(groups = {ValidationGroups.Inster.class, ValidationGroups.Update.class}, message = "校验码key不能为空")
 
     private String checkcodekey;
 
     /**
      * 邮箱
      */
-    @NotEmpty(message = "邮箱不能为空")
+    @NotEmpty(groups = {ValidationGroups.Inster.class}, message = "邮箱不能为空")
 
     private String email;
     /**
      * 昵称
      */
-    @NotEmpty(message = "昵称不能为空")
+    @NotEmpty(groups = {ValidationGroups.Inster.class}, message = "昵称不能为空")
 
     private String nickname;
     /**
      * 密码
      */
-    @NotEmpty(message = "密码不能为空")
+    @NotEmpty(groups = {ValidationGroups.Inster.class, ValidationGroups.Update.class}, message = "密码不能为空")
 
     private String password;
     /**
      * 确认密码
      */
-    @NotEmpty(message = "确认密码不能为空")
+    @NotEmpty(groups = {ValidationGroups.Inster.class, ValidationGroups.Update.class}, message = "确认密码不能为空")
 
     private String confirmpwd;
     /**
      * 用户名
      */
-    @NotEmpty(message = "用户名不能为空")
+    @NotEmpty(groups = {ValidationGroups.Inster.class}, message = "用户名不能为空")
 
     private String username;
 
