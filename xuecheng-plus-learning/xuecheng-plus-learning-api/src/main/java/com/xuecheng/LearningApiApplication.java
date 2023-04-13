010c2976c612bf5544ca742ac3f4715a5e9c4c90
woldier <1098582358@qq.com>
2023-04-13 11:44:03 +0800

    learning-api LearningApiApplication 添加开启AspectJ 暴露

--- xuecheng-plus-learning/xuecheng-plus-learning-api/src/main/java/com/xuecheng/LearningApiApplication.java
+++ xuecheng-plus-learning/xuecheng-plus-learning-api/src/main/java/com/xuecheng/LearningApiApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @EnableFeignClients(basePackages={"com.xuecheng.*.feignclient"})
 @SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class LearningApiApplication {
 
     public static void main(String[] args) {
